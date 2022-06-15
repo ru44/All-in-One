3941b90b1267da37f000fd145bd30046abdbec74
RuM <rmahxhook@gmail.com>
2022-06-15 18:26:01 +0300

    Add DotNet - Spring - Svelte

---
+++ backendSpring/src/test/java/com/backendSpring/backendSpring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendSpring.backendSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
